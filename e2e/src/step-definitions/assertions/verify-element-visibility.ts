256efc82f9727c4d223317e54e27028188f1bcad
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-01 05:33:37 +0800

    custom retries implemented

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -6,26 +6,6 @@ import {ScenarioWorld} from '../setup/world';
 import {waitFor} from '../../support/wait-for-behavior';
 
 
-     Then(
-             /^the "([^"]*)" should contain the text "(.*)"$/,
-             async function(this: ScenarioWorld, elementKey:ElementKey, expectedElementText:string) {
-                 const {
-                     screen: { page },
-                     globalConfig,
-                     globalVariables,
-                 } = this;
-
-                 console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
-
-                 const elementIdentifier = getElementLocator(page, elementKey, globalVariables, globalConfig);
-
-                     const content = await page.textContent(elementIdentifier)
-
-                         //expect(content).toBe(expectedElementText);
-
-                     }
-         )
-
     Then(
         /^the "([^"]*)" should be displayed$/,
         async function(this: ScenarioWorld, elementKey:string){
