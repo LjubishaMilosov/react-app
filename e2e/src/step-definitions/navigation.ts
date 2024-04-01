f92b758aa0cea56f4b63479eb02f3fdfa17ab80f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-02 05:51:10 +0800

    determining pageId by matching url to page regex- final

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -2,6 +2,7 @@ import { Given } from '@cucumber/cucumber'
 import {PageId} from "../env/global";
 import{ navigateToPage, currentPathMatchesPageId} from'../support/navigation-behavior'
 import {waitFor} from "../support/wait-for-behavior";
+import {ScenarioWorld} from "./setup/world";
 
 
      Given(
@@ -20,3 +21,17 @@ import {waitFor} from "../support/wait-for-behavior";
                         await waitFor(() => currentPathMatchesPageId(page, pageId, globalConfig));
                  }
          )
+
+Given(
+    /^I am directed to the "([^"]*)" page$/,
+    async function(this:ScenarioWorld, pageId: PageId) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`I am directed to the ${pageId} page`)
+
+        await waitFor( () => currentPathMatchesPageId(page, pageId, globalConfig))
+    }
+)
